99e9e4090dc281693e525acb89b64c7a135fef23
phuvinh010701 <nguyenphuvinh01@gmail.com>
2022-10-28 16:31:42 +0700

    try with resnet50

--- model.py
+++ model.py
@@ -1,45 +1,18 @@
 from torch import nn
 import torch
-
+from torchvision import models
 class siamese(nn.Module):
     def __init__(self):
         super(siamese, self).__init__()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(1, 96, kernel_size=11,stride=1),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-            nn.MaxPool2d(3, stride=2),
-            
-            nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
-            nn.MaxPool2d(3, stride=2),
-            nn.Dropout2d(p=0.3),
-
-            nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
-            nn.ReLU(inplace=True),
-            
-            nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, stride=2),
-            nn.Dropout2d(p=0.3),
-        )
-        self.fc = nn.Sequential(
-            nn.Linear(30976, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(p=0.5),
-            
-            nn.Linear(1024, 256),
-            nn.ReLu(inplace=True),
-
-            nn.Linear(256, 128)
-
+        
+        self.base_model = models.resnet50(weights=True)
+        self.base_model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.base_model.fc = nn.Sequential(
+            nn.Linear(2048, 128)
         )
         
     def forward_once(self, x):
-        output = self.cnn(x)
-        output = output.view(output.size(0), -1)
-        output = self.fc(output)
+        output = self.base_model(x)
         return output
 
     def forward(self, input1, input2):
@@ -49,7 +22,7 @@ class siamese(nn.Module):
 
 class contrastive_loss(nn.Module):
 
-    def __init__(self, margin=2.0):
+    def __init__(self, margin=1.0):
         super(contrastive_loss, self).__init__()
         self.margin = margin
 
